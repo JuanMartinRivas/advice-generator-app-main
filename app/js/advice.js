241589fe668b611f8ab6105b91eff535be43c628
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-12-30 09:39:15 -0300

    Finishing advice script

--- app/js/advice.js
+++ app/js/advice.js
@@ -2,17 +2,18 @@ const idNumber = document.querySelector('.id-number');
 const adviceText = document.querySelector('.text');
 const diceBtn = document.querySelector('.dice-btn');
 
-fetch('https://api.adviceslip.com/advice', { mode: 'cors' })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(response){
-        console.log(response.slip.advice)
-    })
-
 diceBtn.addEventListener('click', handleClick);
 
 function handleClick() {
-    idNumber.textContent = '115';
-    adviceText.textContent = 'Oranges are orange';
+    fetch('https://api.adviceslip.com/advice', { cache: "no-cache" }, {mode: "cors"})
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            idNumber.textContent = `${response.slip.id}`;
+            adviceText.textContent = `${response.slip.advice}`;
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
